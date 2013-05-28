ae12284149c80003d69eeb5c8c0f71d208832e59
jbadeau <jose.badeau@gmail.com>
2013-05-28 22:58:58 +0200

    removed webjars paths, migrated to hello app and added packages

--- seed/seed-webapp/src/main/webapp/app/run.js
+++ seed/seed-webapp/src/main/webapp/app/run.js
@@ -1,33 +1,18 @@
 (function(curl) {
 
 	var config = {
-		// baseUrl: '',
-		//paths: {
-			// Configure paths here
-		//},
 		packages: [
-			// Define application-level packages
-			{ name: 'welcome', location: 'static/lib/welcome' },
-
-			// Define a theme package, and configure it to always use the css module loader
-			// No need to use AMD 'css!' plugin to load things in this package, it will happen
-			// automatigally.
-			// WARNING: The moduleLoader config syntax will be changing in an upcoming version
-			// of curl.
-			{ name: 'theme', location: 'theme', config: { moduleLoader: 'curl/plugin/css' } },
-
+			{ name: 'seed', location: 'static/seed' },
 			// Add third-party packages here
-			{ name: 'curl', location: 'static/webjars/curl/0.7.3/src/curl' },
-			{ name: 'wire', location: 'static/webjars/wire/0.9.4', main: 'wire' },
-			{ name: 'cola', location: 'static/webjars/cola/0.1.0-SNAPSHOT', main: 'cola' },
-			{ name: 'when', location: 'static/webjars/when/2.0.1', main: 'when' },
-			{ name: 'meld', location: 'static/webjars/meld/1.3.0', main: 'meld' },
-			{ name: 'poly', location: 'static/webjars/poly/0.5.1' }
+			{ name: 'curl', location: 'static/lib/curl/src/curl' },
+			{ name: 'wire', location: 'static/lib/wire', main: 'wire' },
+			{ name: 'cola', location: 'static/lib/cola', main: 'cola' },
+			{ name: 'when', location: 'static/lib/when', main: 'when' },
+			{ name: 'meld', location: 'static/lib/meld', main: 'meld' },
+			{ name: 'poly', location: 'static/lib/poly' }
 		],
 		// Polyfill everything ES5-ish
 		preloads: ['poly/all']
-		// Or, select individual polyfills if you prefer
-		//preloads: ['poly/array', 'poly/function', 'poly/json', 'poly/object', 'poly/string', 'poly/xhr']
 	};
 
 	curl(config, ['wire!app/main']);
