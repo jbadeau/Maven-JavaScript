7ccb93cb4aedd0234a1e5d384b66baa750400c17
jbadeau <jose.badeau@gmail.com>
2013-05-28 21:15:47 +0200

    unittests now run without unpacking

--- seed/seed-javascript/src/main/javascript/Foo.js
+++ seed/seed-javascript/src/main/javascript/Foo.js
@@ -1,3 +1,8 @@
-function foo() {
-	
-}
+// example.js
+define(function() {
+  return {
+    hello : function(name) {
+      return "hello " + name;
+    };
+  };
+});
---
+++ seed/seed-javascript/src/test/javascript/FooSpec.js
@@ -0,0 +1,8 @@
+// example.spec.js
+define(["when"], function(when) {
+	describe("example.hello", function() {
+		it("should return 'hello jim'", function() {
+			expect("hello jim").toBe("hello jim");
+		});
+	});
+});
