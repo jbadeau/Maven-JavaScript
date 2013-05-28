3bd9699032a389877b6af866046c2482b191d6c9
jbadeau <jose.badeau@gmail.com>
2013-05-28 10:04:36 +0200

    migrated to webjars format

--- seed/seed-javascript/bin/target/test-classes/welcome/ExampleSpec.js
+++
@@ -1,7 +0,0 @@
-define("welcome/Example", function(Example) {
-	describe("example.hello", function() {
-		it("should return 'hello jim'", function() {
-			expect(Example.hello("jim")).toBe("hello jim");
-		});
-	});
-});
--- seed/seed-webapp/src/main/webapp/app/run.js
+++ seed/seed-webapp/src/main/webapp/app/run.js
@@ -17,12 +17,12 @@
 			{ name: 'theme', location: 'theme', config: { moduleLoader: 'curl/plugin/css' } },
 
 			// Add third-party packages here
-			{ name: 'curl', location: 'static/lib/curl/src/curl' },
-			{ name: 'wire', location: 'static/lib/wire', main: 'wire' },
-			{ name: 'cola', location: 'static/lib/cola', main: 'cola' },
-			{ name: 'when', location: 'static/lib/when', main: 'when' },
-			{ name: 'meld', location: 'static/lib/meld', main: 'meld' },
-			{ name: 'poly', location: 'static/lib/poly' }
+			{ name: 'curl', location: 'static/webjars/curl/0.7.3/src/curl' },
+			{ name: 'wire', location: 'static/webjars/wire/0.9.4', main: 'wire' },
+			{ name: 'cola', location: 'static/webjars/cola/0.1.0-SNAPSHOT', main: 'cola' },
+			{ name: 'when', location: 'static/webjars/when/2.0.1', main: 'when' },
+			{ name: 'meld', location: 'static/webjars/meld/1.3.0', main: 'meld' },
+			{ name: 'poly', location: 'static/webjars/poly/0.5.1' }
 		],
 		// Polyfill everything ES5-ish
 		preloads: ['poly/all']
